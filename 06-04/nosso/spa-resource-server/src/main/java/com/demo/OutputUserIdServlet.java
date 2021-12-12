eb31b22adff8f2b72037abc4c9f75198afb3592c
yagiaoskyworker <yagiaoskyworker@gmail.com>
2021-12-12 18:53:03 +0900

    Commit latest modules in 06-04

---
+++ 06-04/nosso/spa-resource-server/src/main/java/com/demo/OutputUserIdServlet.java
@@ -0,0 +1,29 @@
+package com.demo;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.security.Principal;
+
+@WebServlet("/getUserId")
+public class OutputUserIdServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        // キャッシュの無効化
+        response.setHeader("progma","no-cache");
+        response.setHeader("Cache-Control","no-cache");
+
+        // 認証済みユーザーを取得
+        Principal userPrincipal = request.getUserPrincipal();
+
+        response.setContentType("application/json; charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        out.write("{");
+        out.printf("  \"method\" : \"%s\",", request.getMethod());
+        out.printf("  \"userId\" : \"%s\"", (userPrincipal != null ? userPrincipal.getName() : ""));
+        out.write("}");
+    }
+}
